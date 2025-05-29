b3a130d9a3a57d1c700564565e8706fc2758cad6
MohammadrezaRabiepour <M.rabie1384@yahoo.com>
2025-05-29 07:05:59 -0700

    update

--- license.py
+++ license.py
@@ -11,25 +11,25 @@ class License:
         print(f"{self.id} {self.name} {self.family} {self.license_number} saved")
 
 
-    def license_validator(self,license):
+    def license_validator(self):
         errors = []
-        if not (type(license[0]) == int and license[0] > 0):
+        if not (type(self.id) == int and self.id > 0):
             errors.append('Person ID must be an integer > 0')
 
-        if not (type(license[1]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", license[1])):
+        if not (type(self.name) == str and re.match(r"^[a-zA-Z\s]{3,30}$", self.name)):
             errors.append('Person Name is Invalid')
 
-        if not (type(license[2]) == str and re.match(r"^[a-zA-Z\s]{3,30}$", license[2])):
+        if not (type(self.family) == str and re.match(r"^[a-zA-Z\s]{3,30}$", self.family)):
             errors.append('Person Family is Invalid')
 
-        if not (type(license[3]) == str and re.match(r"^\d{8}$", license[3])):
+        if not (type(self.license_number) == str and re.match(r"^\d{8}$", self.license_number)):
             errors.append('Person License Number is Invalid')
 
-        if not (type(license[4]) == str and re.match(r"^\d{4}[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12][0-9]|3[01])$",
-                                                     license[4])):
+        if not (type(self.data) == str and re.match(r"^\d{4}[-/](0[1-9]|1[0-2])[-/](0[1-9]|[12][0-9]|3[01])$",
+                                                     self.data)):
             errors.append('Issue Date')
 
-        if not (type(license[5]) == str and re.match(r"^[a-zA-Z\s\d]{2,15}$", license[5])):
+        if not (type(self.license_type) == str and re.match(r"^[a-zA-Z\s\d]{2,15}$", self.license_type)):
             errors.append('lisense is Invalid')
 
         return errors
